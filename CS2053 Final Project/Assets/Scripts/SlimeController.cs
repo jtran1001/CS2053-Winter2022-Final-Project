b7ddab261866e4210470e20376d41d461d577c84
armcaffee <antrmcaffee@gmail.com>
2022-03-16 17:47:24 -0300

    The slime can now move and jump

--- CS2053 Final Project/Assets/Scripts/SlimeController.cs
+++ CS2053 Final Project/Assets/Scripts/SlimeController.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class SlimeController : MonoBehaviour
 {
+
+    public int speed;
+    public float fallMultiplier = 1.5f;
+    public float lowJumpMultiplier = 2f;
+    public float jumpVelocity;
+
+    Rigidbody2D rb;
+
+    private Vector3 horizontalVelocity;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        horizontalVelocity = new Vector3(0f, 0f, 0f);
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalVelocity = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+
+
+        if (Input.GetButtonUp("Jump")) {
+            rb.velocity = Vector2.up * jumpVelocity;
+        }
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && Input.GetButton("Jump")) {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier) * Time.deltaTime;
+        }
+
+        transform.position = transform.position + horizontalVelocity * Time.deltaTime * speed;
     }
 }
