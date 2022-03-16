8bfd3d67d8bae2f3763709b2f35576603cbe8dc0
jtran1001 <dtran@unb.ca>
2022-03-16 15:33:52 -0300

    We have light follow mouse boissss

---
+++ CS2053 Final Project/Assets/LightController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class MouseLightController : MonoBehaviour
+{
+    Vector3 mousePosition;
+    public float moveSpeed = 0.1f;
+    Vector2 position = new Vector2(0f, 0f);
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(position);
+    }
+}
