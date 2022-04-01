f0ce5d7765233c5d32b1c7799f981e0ee12890e7
armcaffee <antrmcaffee@gmail.com>
2022-03-31 22:03:04 -0300

    Played with the light and scenery

--- CS2053 Final Project/Assets/Scripts/LightController.cs
+++ CS2053 Final Project/Assets/Scripts/LightController.cs
@@ -1,25 +1,46 @@
 ﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MouseLightController : MonoBehaviour
 {
-    Vector3 mousePosition;
+    private Vector3 mousePosition;
     public float moveSpeed = 0.1f;
-    Vector2 position = new Vector2(0f, 0f);
-    Rigidbody2D rb;
+    private Vector2 position = new Vector2(0f, 0f);
+    private Rigidbody2D rb;
+    public SlimeController Slime;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        position = Slime.transform.position;
+
     }
     void Update()
     {
-        mousePosition = Input.mousePosition;
-        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
-    }
 
-    void FixedUpdate()
-    {
+        if (Input.GetButton("Fire1"))
+        {
+            mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+        }
+        else
+        {
+            position = Vector2.Lerp(transform.position, Slime.transform.position, moveSpeed);
+        }
         rb.MovePosition(position);
     }
+    
+    void FixedUpdate()
+    {
+        
+        
+        
+
+    }
+
 }
