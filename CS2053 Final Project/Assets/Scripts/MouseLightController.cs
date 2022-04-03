b0817eabe3dd2330afb5fc72baa1ed325a91f7a8
jtran1001 <dtran@unb.ca>
2022-04-02 22:01:50 -0300

    InitEnemyAICommit

--- CS2053 Final Project/Assets/Scripts/MouseLightController.cs
+++ CS2053 Final Project/Assets/Scripts/MouseLightController.cs
@@ -1,18 +1,46 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MouseLightController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Vector3 mousePosition;
+    public float moveSpeed = 0.1f;
+    private Vector2 position = new Vector2(0f, 0f);
+    private Rigidbody2D rb;
+    public SlimeController Slime;
+
+
     void Start()
     {
-        
-    }
+        rb = GetComponent<Rigidbody2D>();
+        position = Slime.transform.position;
 
-    // Update is called once per frame
+    }
     void Update()
     {
-        
+
+        if (Input.GetButton("Fire1"))
+        {
+            mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+        }
+        else
+        {
+            position = Vector2.Lerp(transform.position, Slime.transform.position, moveSpeed);
+        }
+        rb.MovePosition(position);
     }
+
+    void FixedUpdate()
+    {
+
+
+
+
+    }
+
 }
