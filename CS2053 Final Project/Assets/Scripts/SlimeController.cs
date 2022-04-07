b4a4b7d1ab6bcca4efbdda0d011634db1fc3bcda
Matthew Hunter <matthewhunter16@gmail.com>
2022-04-07 09:59:28 -0300

    Snake Work

--- CS2053 Final Project/Assets/Scripts/SlimeController.cs
+++ CS2053 Final Project/Assets/Scripts/SlimeController.cs
@@ -1,18 +1,106 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SlimeController : MonoBehaviour
 {
+
+    public int speed;
+    public float fallMultiplier = 1.5f;
+    public float lowJumpMultiplier = 2f;
+    public float jumpVelocity;
+    public Text HydrationText;
+
+    Rigidbody2D rb;
+
+    private Vector3 horizontalVelocity;
+    private bool CanJump = false;
+    private bool WaterZone = false;
+    private int Hydration = 10;
+    private int FullHydration = 10;
+    private float LastResetTime = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        horizontalVelocity = new Vector3(0f, 0f, 0f);
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        horizontalVelocity = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+
+        if (CanJump is true)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                rb.velocity = Vector2.up * jumpVelocity;
+            }
+            if (rb.velocity.y < 0)
+            {
+                rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier) * Time.deltaTime;
+            }
+            else if (rb.velocity.y > 0 && Input.GetButton("Jump"))
+            {
+                rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier) * Time.deltaTime;
+            }
+        }
         
+
+        transform.position = transform.position + horizontalVelocity * Time.deltaTime * speed;
+
+        if (WaterZone is false)
+        {
+            Hydration = FullHydration - ((int)Time.timeSinceLevelLoad - (int)LastResetTime);
+
+        }
+        else 
+        {
+            Hydration = 10; 
+        }
+        HydrationText.text = "Hydration: " + Hydration.ToString();
+
+    }
+
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        
+        if (c.gameObject.tag == "CanJumpGround")
+        {
+            CanJump = true;
+        }
+
+        if (c.gameObject.tag == "Enemy_Snake")
+        {
+            //die
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D c)
+    {
+        if (c.gameObject.tag == "WaterZone")
+        {
+            WaterZone = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D c)
+    {
+
+        if (c.gameObject.tag == "CanJumpGround")
+        {
+            CanJump = false;
+        }
+
+        if (c.gameObject.tag == "WaterZone")
+        {
+            WaterZone = false;
+            LastResetTime = Time.timeSinceLevelLoad;
+        }
     }
 }
---
+++ CS2053 Final Project/Assets/Scripts/SnakeAI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeAI : MonoBehaviour
+{
+    private Animator anim;
+    private Vector3 velocity;
+    private SpriteRenderer rend;
+    public float speed = 2.0f;
+    public float leftEdge = 0.0f;
+    public float rightEdge = 5.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int startingMove = Random.Range(0,1);
+        Vector3 velocity = new Vector3(1, 0f, 0f);
+        rend = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var dist = (transform.position - Camera.main.transform.position).z;
+        /*var leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, dist)).x;
+        var rightBorder = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, dist)).x;
+        var bottomBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, dist)).y;
+        var topBorder = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, dist)).y;
+*/
+        float width = rend.bounds.size.x;
+        float height = rend.bounds.size.y;
+
+        if((transform.position.x <= leftEdge) && velocity.x < 0f){
+            velocity = new Vector3(speed, 0f, 0f);
+        }
+        if((transform.position.x >= rightEdge)){
+            velocity = new Vector3(-speed, 0f, 0f);
+        }
+
+        transform.position = transform.position + velocity * Time.deltaTime * speed;
+    }
+
+    void LateUpdate(){
+        if(velocity.x >= 0){
+            anim.Play("SnakeRight");
+        }
+        if(velocity.x < 0){
+            anim.Play("SnakeLeft");
+        }
+    }
+}
