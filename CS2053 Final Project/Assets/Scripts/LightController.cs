2fcb76e64c72d8760a8af1d42e5dd7a7dd90c0f4
armcaffee <antrmcaffee@gmail.com>
2022-04-09 13:17:23 -0300

    Scene updates

--- CS2053 Final Project/Assets/Scripts/LightController.cs
+++ CS2053 Final Project/Assets/Scripts/LightController.cs
@@ -7,33 +7,83 @@ using UnityEngine.SceneManagement;
 public class MouseLightController : MonoBehaviour
 {
     private Vector3 mousePosition;
-    public float moveSpeed = 0.1f;
+    public float moveSpeed = 0.05f;
     private Vector2 position = new Vector2(0f, 0f);
     private Rigidbody2D rb;
     public SlimeController Slime;
 
+    private int State;
+
+    private float startTime;
+    private float journeyLength;
+
+    public Text Lilae;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         position = Slime.transform.position;
-
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(transform.position, Slime.transform.position);
+        State = 0;
+        Lilae.text = "";
     }
     void Update()
     {
 
-        if (Input.GetButton("Fire1"))
+        if (State == 0)
         {
-            mousePosition = Input.mousePosition;
-            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-            position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
-            rb.MovePosition(position);
+            float distCovered = (Time.time - startTime) * moveSpeed;
+            float fractionOfJourney = distCovered / journeyLength;
+            position = Vector2.Lerp(transform.position, Slime.transform.position, fractionOfJourney);
+
+            if (transform.position.y < -25)
+            {
+                State = 1;                
+            }
+
         }
-        else
+
+        if (State == 1)
         {
-            position = Vector2.Lerp(transform.position, Slime.transform.position, moveSpeed);
+            Lilae.text = "HELLO! SHOULDN'T A LITTLE SLIME LIKE YOU BE PLAYING IN A SWAMP WITH YOUR FRIENDS?";
+            if (Input.GetButton("Jump"))
+            {
+                State = 2;
+                
+            }
+
         }
+
+        if (State == 2)
+        {
+
+            Lilae.text = "AWW, THAT'S SO SAD! HERE, LET ME HELP YOU FIND A WAY OUT.";
+            if (Input.GetButton("Jump"))
+            {
+                State = 3;
+                Lilae.text = "AWW, THAT'S SO SAD! HERE, LET ME HELP YOU FIND A WAY OUT.";
+            }
+        }
+
         
+
+        if (State == 3)
+        {
+            if (Input.GetButton("Fire1"))
+            {
+                mousePosition = Input.mousePosition;
+                mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+                position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+                rb.MovePosition(position);
+                Lilae.text = "";
+            }
+            else
+            {
+                position = Vector2.Lerp(transform.position, Slime.transform.position, moveSpeed);
+            }
+        }
+
     }
     
     void LateUpdate()
--- CS2053 Final Project/Assets/Scripts/SlimeController.cs
+++ CS2053 Final Project/Assets/Scripts/SlimeController.cs
@@ -22,6 +22,9 @@ public class SlimeController : MonoBehaviour
     public int FullHydration = 10;
     private int Hydration = 10;
     private float LastResetTime = 0;
+    private int State;
+
+    SpriteRenderer m_SpriteRenderer;
 
     // Start is called before the first frame update
     void Start()
@@ -29,43 +32,75 @@ public class SlimeController : MonoBehaviour
         horizontalVelocity = new Vector3(0f, 0f, 0f);
         rb = GetComponent<Rigidbody2D>();
         Hydration = FullHydration;
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+        m_SpriteRenderer.color = new Color(0, 255, 0, 255);
+        State = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalVelocity = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-
-        if (CanJump is true)
-        {
-            if (Input.GetButtonDown("Jump"))
-            {
-                rb.velocity = Vector2.up * jumpVelocity;
-            }
-            if (rb.velocity.y < 0)
-            {
-                rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier) * Time.deltaTime;
-            }
-            else if (rb.velocity.y > 0 && Input.GetButton("Jump"))
-            {
-                rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier) * Time.deltaTime;
-            }
-        }
-        
-
-        transform.position = transform.position + horizontalVelocity * Time.deltaTime * speed;
 
         if (WaterZone is false)
         {
             Hydration = FullHydration - ((int)Time.timeSinceLevelLoad - (int)LastResetTime);
-
         }
-        else 
+        else
         {
-            Hydration = FullHydration; 
+            Hydration = FullHydration;
         }
         HydrationText.text = "Hydration: " + Hydration.ToString();
 
+
+        if (Hydration < 0)
+        {
+            m_SpriteRenderer.color = new Color(0, 0, 0, 0);
+            State = 2;
+        }
+        else if (Hydration <= 4)
+        {
+            m_SpriteRenderer.color = new Color(255, 0, 0, 255);
+        }
+        else if (Hydration <= 8)
+        {
+            m_SpriteRenderer.color = new Color(255, 128, 0, 255);
+        }
+        else if (Hydration <= 12)
+        {
+            m_SpriteRenderer.color = new Color(255, 255, 0, 255);
+        }
+        else if (Hydration <= 16)
+        {
+            m_SpriteRenderer.color = new Color(128, 255, 0, 255);
+        }
+        else
+        {
+            m_SpriteRenderer.color = new Color(0, 255, 0, 255);
+        }
+
+        if (State == 1)
+        {
+            horizontalVelocity = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+
+            if (CanJump is true)
+            {
+                if (Input.GetButtonDown("Jump"))
+                {
+                    rb.velocity = Vector2.up * jumpVelocity;
+                }
+                if (rb.velocity.y < 0)
+                {
+                    rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier) * Time.deltaTime;
+                }
+                else if (rb.velocity.y > 0 && Input.GetButton("Jump"))
+                {
+                    rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier) * Time.deltaTime;
+                }
+            }
+
+
+            transform.position = transform.position + horizontalVelocity * Time.deltaTime * speed;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D c)
