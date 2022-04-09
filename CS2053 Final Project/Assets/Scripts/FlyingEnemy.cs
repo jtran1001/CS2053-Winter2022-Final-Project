be56849a08290555ee4b8cd01d80f1f653e1302c
jtran1001 <dtran@unb.ca>
2022-04-09 20:58:57 -0300

    Commit

---
+++ CS2053 Final Project/Assets/Scripts/FlyingEnemy.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEnemy : MonoBehaviour
+{
+    public float speed;
+    private GameObject player;
+    public bool chase = false;
+    public Transform originPoint;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        if(player == null)
+        {
+            return;
+        }
+        if (chase == true)
+        {
+            Chase();
+        }
+        else
+        {
+            ReturnOriginPoint();
+        }
+        Flip();
+    }
+
+    private void Chase()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+    }
+
+    private void ReturnOriginPoint()
+    {
+        transform.position = Vector2.MoveToward(transform.position, startingPoint.position, speed * Time.deltaTime);
+    }
+
+    private void Flip()
+    {
+        if(transform.position.x > player.transform.position.x)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+    }
+}
