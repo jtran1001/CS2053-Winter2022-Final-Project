d8aa162289f05b54cda0731a0842636c36c5d22f
armcaffee <antrmcaffee@gmail.com>
2022-04-09 22:44:59 -0300

    Start, Level1, and End screens are done

--- CS2053 Final Project/Assets/Scripts/LightController.cs
+++ CS2053 Final Project/Assets/Scripts/LightController.cs
@@ -49,7 +49,7 @@ public class LightController : MonoBehaviour
 
         if (State == 1)
         {
-            Lilae.text = "HELLO! SHOULDN'T A LITTLE SLIME LIKE YOU BE PLAYING IN A SWAMP WITH YOUR FRIENDS?";
+            Lilae.text = "\"HELLO! SHOULDN'T A LITTLE SLIME LIKE YOU BE PLAYING IN A SWAMP WITH YOUR FRIENDS?\"";
             Instructions.text = "Press SPACE to jump/relpy.";
             if (Input.GetButton("Jump"))
             {
@@ -62,7 +62,7 @@ public class LightController : MonoBehaviour
         if (State == 2)
         {
 
-            Lilae.text = "AWW, THAT'S SO SAD! HERE, LET ME HELP YOU FIND A WAY OUT.";
+            Lilae.text = "\"AWW, THAT'S SO SAD! HERE, LET ME HELP YOU FIND A WAY OUT.\"";
             Instructions.text = "Left-click on the mouse to direct Lilae.";
 
             if (Input.GetButton("Fire1"))
